6b8ee268d68fef521dd5e5d70a1b683314731ff0
kishikawakatsumi <kishikawakatsumi@mac.com>
2012-09-29 09:27:27 +0900

    Changed file caches lifetime.

--- ClassicMap/WMAppDelegate.m
+++ ClassicMap/WMAppDelegate.m
@@ -15,16 +15,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     [[AFNetworkActivityIndicatorManager sharedManager] setEnabled:YES];
-    [self registerDefaultsFromSettingsBundle];
     
-    NSUserDefaults *defs = [NSUserDefaults standardUserDefaults];
-    BOOL clearAllFileCachesOnLaunch = [defs boolForKey:@"clearAllFileCachesOnLaunch"];
-    if (clearAllFileCachesOnLaunch) {
-        NSLog(@"%@", @"clear caches");
-        [[WMImageCache sharedInstance] deleteAllCacheFiles];
-        [defs setBool:NO forKey:@"clearAllFileCachesOnLaunch"];
-        [defs synchronize];
-    }
+    [[WMImageCache sharedInstance] deleteAllCacheFiles];
     
     return YES;
 }
@@ -34,33 +26,8 @@
     [[WMImageCache sharedInstance] purgeMemoryCache];
 }
 
-- (void)registerDefaultsFromSettingsBundle
-{
-    NSUserDefaults * defs = [NSUserDefaults standardUserDefaults];
-    [defs synchronize];
-    
-    NSString *settingsBundle = [[NSBundle mainBundle] pathForResource:@"Settings" ofType:@"bundle"];
-    if (!settingsBundle) {
-        return;
-    }
-    
-    NSDictionary *settings = [NSDictionary dictionaryWithContentsOfFile:[settingsBundle stringByAppendingPathComponent:@"Root.plist"]];
-    NSArray *preferences = [settings objectForKey:@"PreferenceSpecifiers"];
-    NSMutableDictionary *defaultsToRegister = [[NSMutableDictionary alloc] initWithCapacity:[preferences count]];
-    
-    for (NSDictionary *prefSpecification in preferences) {
-        NSString *key = [prefSpecification objectForKey:@"Key"];
-        if (key) {
-            id currentObject = [defs objectForKey:key];
-            if (!currentObject) {
-                id objectToSet = [prefSpecification objectForKey:@"DefaultValue"];
-                [defaultsToRegister setObject:objectToSet forKey:key];
-            }
-        }
-    }
-    
-    [defs registerDefaults:defaultsToRegister];
-    [defs synchronize];
+- (void)applicationWillTerminate:(UIApplication *)application {
+    [[WMImageCache sharedInstance] deleteAllCacheFiles];
 }
 
 @end
